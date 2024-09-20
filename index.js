5f8e1469c6cd015f60e6599db301bde261455997
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-09-20 11:58:53 +0700

    feat/init project

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const process = require('process');
+const app = express();
+
+// DOTENV CONFIG
+dotenv.config();
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+// CORS
+app.use(cors());
+
+// DB CONNECTION
+if (!process.env.MONGODB_URI) {
+  throw Error('Database connection string not found');
+}
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Successfully connected to the database'); 
+  })
+  .catch((err) => {
+    console.log('Failed to connect to the database');
+    console.log(err);
+  })
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Hello from PAW 10 Backend Service!');
+});
+
+// APP START
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
